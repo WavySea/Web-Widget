7e7a3c24bd11ff4184d2906f6fa3e76c15a65dd3
Haoyan Su <hs2949@nyu.edu>
2017-07-19 15:54:40 +0800

    Update checkLoopTime.js

--- checkLoopTime.js
+++ checkLoopTime.js
@@ -1,6 +1,6 @@
-var origincode = " while(a>4){a += 1;}";
+var origincode = " while(1>0){a = 3;}";
 var numWhile  = origincode.search(/while/);//1
-var newcode1 = origincode.replace(/while/, "var startTime=new Date();var a; while"); //插入了start声明
+var newcode1 = origincode.replace(/while/, "var startTime=new Date();while"); //插入了start声明
 var lastHalfCode = origincode.substring(numWhile+5);//返回while之后的字符串
 var newnumWhile = newcode1.search(/while/)+5; //插入start声明后while中的e所在位置
 var firstHalfCode = newcode1.substring(1,newnumWhile);//截取插入start声明后的前半部分字符串，直到while
